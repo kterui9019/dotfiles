62f502cdff4f831d8eafb8c69a4bf34708b4cdec
Kazuki Terui <pleasure.teru@gmail.com>
2022-03-21 22:42:28 +0900

    fix .config operation not permited error

--- .bin/link.sh
+++ .bin/link.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# symlinkでない.configを直接symlinkに書き換えようとするとOSに怒られるので退避
+[[ -e "${HOME}/.config" ]] && [[ ! -L "${HOME}/.config" ]] && mv "${HOME}/.config" "${HOME}/.config.bak"
+
 for dotfile in "${SCRIPT_DIR}"/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
