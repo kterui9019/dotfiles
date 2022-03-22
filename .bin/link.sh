ed749fdca62af1cc97aace42c44813bd3de178d6
Kazuki Terui <pleasure.teru@gmail.com>
2022-03-22 21:53:27 +0900

    add .config

--- .bin/link.sh
+++ .bin/link.sh
@@ -6,18 +6,15 @@ for dotfile in "${SCRIPT_DIR}"/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
-    # TODO: ln: /Users/$username/.config: Operation not permitted
-    [[ "$dotfile" == "${SCRIPT_DIR}/.config" ]] && continue
 
-    # TODO: ln: /Users/$username/.config/$dirname: Operation not permitted
-    # if [ "$dotfile" == "${SCRIPT_DIR}/.config" ] ; then
-    #     mkdir -p "$HOME/.config"
-    #     for configfile in "${SCRIPT_DIR}"/.config/* ; do
-    #         ln -fnsv "$configfile" "$HOME/.config"
-    #     done
+    if [ "$dotfile" == "${SCRIPT_DIR}/.config" ] ; then
+        mkdir -p "$HOME/.config"
+        for configfile in "${SCRIPT_DIR}"/.config/* ; do
+            ln -fnsv "$configfile" "$HOME/.config"
+        done
 
-    #     continue
-    # fi
+        continue
+    fi
 
     ln -fnsv "$dotfile" "$HOME"
 done
