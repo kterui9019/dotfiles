1002b10ae1dd5f050470ee420b194c07bef4ba40
Kazuki Terui <terui.k@t2.auto>
2024-07-06 17:53:13 +0900

    add if

--- bin/brew.sh
+++ bin/brew.sh
@@ -5,11 +5,13 @@
 # 	exit 1
 # fi
 
-ARCH=$(uname -m)
-if [[ $ARCH == arm64 ]]; then
-    eval $(/opt/homebrew/bin/brew shellenv)
-elif [[ $ARCH == x86_64 ]]; then
-    eval $(/usr/local/bin/brew shellenv)
+if [ "$(uname)" = "Darwin" ] ; then
+    ARCH=$(uname -m)
+    if [[ $ARCH == arm64 ]]; then
+        eval $(/opt/homebrew/bin/brew shellenv)
+    elif [[ $ARCH == x86_64 ]]; then
+        eval $(/usr/local/bin/brew shellenv)
+    fi
 fi
 
 brew bundle --global
