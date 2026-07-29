8dcb9970393849ff92b5b7ce88480e60d0b5bcf0
照井 和希 <terui@bengo4.com>
2026-07-29 12:45:08 +0900

    Neovim keymapsのzellij/nvim-tmux-navigation向け<C-h>上書きを削除
    after/plugin/herdr_nav.luaのマップと競合していた
    (VeryLazyイベントでロードされるためこちらが後勝ちしていた)。
    navigationはherdr_nav.luaに一本化する。

--- bin/.config/nvim/lua/config/keymaps.lua
+++ bin/.config/nvim/lua/config/keymaps.lua
@@ -3,14 +3,6 @@
 -- Add any additional keymaps here
 local Util = require("lazyvim.util")
 
-vim.keymap.del("n", "<C-h>")
-vim.keymap.del("n", "<C-j>")
-vim.keymap.del("n", "<C-k>")
-vim.keymap.del("n", "<C-l>")
-vim.keymap.set("n", "<C-h>", ":ZellijNavigateLeft<CR>", { desc = "Go to Left Window", remap = true, silent = true })
-vim.keymap.set("n", "<C-j>", ":ZellijNavigateDown<CR>", { desc = "Go to Lower Window", remap = true, silent = true })
-vim.keymap.set("n", "<C-k>", ":ZellijNavigateUp<CR>", { desc = "Go to Upper Window", remap = true, silent = true })
-vim.keymap.set("n", "<C-l>", ":ZellijNavigateRight<CR>", { desc = "Go to Right Window", remap = true, silent = true })
 vim.keymap.set("n", "<C-/>", function()
-  Util.terminal(nil, { border = "rounded" })
+  Snacks.terminal(nil, { border = "rounded" })
 end, { desc = "Term with border" })
